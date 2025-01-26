384c2bd863f940340d0e0075b0994c3a7b6d9fff
VladislavNaumets <klinicpanik@yandex.ru>
2025-01-26 22:37:28 +0300

    user migrate

--- backend/constants/user_constants.py
+++ backend/constants/user_constants.py
@@ -4,5 +4,5 @@ EMAIL_MAX_LENGTH = 128
 NAME_MAX_LENGTH = 128
 """Максимальная длина username, first_name, last_name."""
 
-PASSWORD_MAX_LENGTH = 64
+PASSWORD_MAX_LENGTH = 128
 """Максимальная длина пароля."""
