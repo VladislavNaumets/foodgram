8f516bf92a68ce0a9170fe43f6c1976cb0ca615c
VladislavNaumets <klinicpanik@yandex.ru>
2025-02-16 14:09:53 +0300

    url ref

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,6 +17,48 @@ urlpatterns = [
     path("", include(router.urls)),
     path("", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
+    path(
+        "recipes/download_shopping_cart/",
+        RecipeViewSet.as_view({'get': 'download_shopping_cart'}),
+        name="download",
+    ),
+    path(
+        "users/",
+        UserViewSet.as_view({"get": "list", "post": "create"}),
+        name="user-list-create",
+    ),
+    path(
+        "users/<int:pk>/",
+        UserViewSet.as_view({"get": "retrieve"}),
+        name="user-detail",
+    ),
+    path(
+        "recipes/<int:recipe_id>/shopping_cart/",
+        RecipeViewSet.as_view({"post": "create", "delete": "destroy"}),
+        name="shopping_cart",
+    ),
+    path(
+        "recipes/<int:recipe_id>/favorite/",
+        RecipeViewSet.as_view(
+            {'post': 'manage_favorites', 'delete': 'manage_favorites'}),
+        name="favorite",
+    ),
+    path("users/me/avatar/", UserViewSet.as_view(), name="user-avatar"),
+    path(
+        "users/subscriptions/",
+        UserViewSet.as_view(),
+        name="subscriptions",
+    ),
+    path(
+        "users/me/",
+        UserViewSet.as_view({"get": "retrieve"}),
+        name="user-me",
+    ),
+    path(
+        "users/<int:user_id>/subscribe/",
+        UserViewSet.as_view({"post": "create", "delete": "destroy"}),
+        name="subscribe-to",
+    ),
 ]
 
 if settings.DEBUG:
