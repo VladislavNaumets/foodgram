a08a5d1e58181b583dd4522c336a5de813f64e9c
VladislavNaumets <klinicpanik@yandex.ru>
2025-02-16 13:30:33 +0300

    url ref

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,8 +15,8 @@ router.register(r"users", UserViewSet, basename="user")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("auth/", include("djoser.urls")),
-    path("auth/token/", include("djoser.urls.authtoken")),
+    path("", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
 ]
 
 if settings.DEBUG:
